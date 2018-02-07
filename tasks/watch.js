2e9b69686b643a512fab0dbb7b810c289c2ad060
alexandre.araujo <alexandre.araujo@mv.com.br>
2018-02-07 13:14:21 -0300

    public task finished

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,7 +3,7 @@ var connect = require('gulp-connect');
 
 gulp.task('static_serve', function() {
   connect.server({
-    root: ".",
+    root: "./public",
     port: 9001,
     livereload: true
   });
@@ -13,14 +13,13 @@ gulp.task('reload',function(){
 	connect.reload();
 });
 
-
-
-gulp.task('watch',['compile','template','sass'],function(){
+gulp.task('watch',['compile','template','sass','build:public'],function(){
     gulp.run('static_serve');
     gulp.watch("./src/**/*.pug", ['template']);
     gulp.watch('./src/**/*.ts', ['compile']);
     gulp.watch('./src/**/*.scss', ['sass']);
     gulp.watch('./dist/**/*.*').on('change',function(file){
+        gulp.run(['copy:dist','copy:test']);
     	gulp.src(file.path).pipe(connect.reload());
     });
 });
