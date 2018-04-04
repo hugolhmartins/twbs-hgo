9a8df68cfa6fee9fa649fbbf643489bdc4915e28
alexsad <alex.query@gmail.com>
2018-04-04 20:21:57 -0300

    add unsubscribe event to all components

--- src/test/apps/test-switch-slider.ts
+++ src/test/apps/test-switch-slider.ts
@@ -1,6 +1,9 @@
 export class TestSwitchSlider {
     private test: string;
     private refresh: Function;
+    constructor(){
+    	this.test = '';
+    }
     private checkHandler(pValue:string){
         this.test = pValue;
         this.refresh();
--- src/ui/switch-slider/switch-slider.ts
+++ src/ui/switch-slider/switch-slider.ts
@@ -7,13 +7,21 @@ export class SwitchSlider{
     private disabled:string;
     private checkedValue: string;
     private unCheckedValue: string;
+    private checkedColor: string;
+    private unCheckedColor: string;
+    private refresh: Function;
 	constructor(){
         this.type = "square";
         this.checkedValue = "y";
         this.unCheckedValue = "n";
+        this.checkedColor = "#4d7498";
+        this.unCheckedColor = "#af5757";
 	}
 	private set value(value:boolean){
         this.selected = value ? true : false;
 		this.onCheck.emit(this.selected ? this.checkedValue : this.unCheckedValue);
+	}
+	private detached(){
+		this.onCheck.unsubscribeAll();
 	}
 }
