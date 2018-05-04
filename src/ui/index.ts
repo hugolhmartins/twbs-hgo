932bad164f7dfebe0b9816683cf71fb95a4b9b46
alexsad <alex.query@gmail.com>
2018-05-03 22:45:57 -0300

    feat: update to fjw 1.3.1

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,9 +1,9 @@
-import confirm_dialog from "./confirm-dialog/confirm-dialog.pug";
-import loading_bar from "./loading-bar/loading-bar.pug";
-import pagination_box from "./pagination/pagination.pug";
-import date_picker from "./date-picker/date-picker.pug";
-import select_box from "./select-box/select-box.pug";
-import file_input from "./file-input/file-input.pug";
-import date_time_picker from "./date-time-picker/date-time-picker.pug";
-import switch_slider from "./switch-slider/switch-slider.pug";
-export {confirm_dialog,loading_bar,pagination_box,date_picker,select_box,file_input,date_time_picker,switch_slider};
+import confirmDialog from "./confirm-dialog/confirm-dialog.pug";
+import loadingBar from "./loading-bar/loading-bar.pug";
+import paginationBox from "./pagination/pagination.pug";
+import datePicker from "./date-picker/date-picker.pug";
+import selectBox from "./select-box/select-box.pug";
+import fileInput from "./file-input/file-input.pug";
+import dateTimePicker from "./date-time-picker/date-time-picker.pug";
+import switchSlider from "./switch-slider/switch-slider.pug";
+export {confirmDialog,loadingBar,paginationBox,datePicker,selectBox,fileInput,dateTimePicker,switchSlider};
