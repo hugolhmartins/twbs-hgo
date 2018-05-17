c5d5f59f61d0cf4f72c9b7b33fb0642965bb7897
Hugo Leonardo de Holanda Martins <hugo.martins@mv.com.br>
2018-05-17 14:11:51 -0300

    Version 0.11.3 Add accept in file-input

--- src/ui/file-input/file-input.ts
+++ src/ui/file-input/file-input.ts
@@ -4,6 +4,11 @@ export class FileInput{
     public onSelect = new EventEmitter<Blob>();
     public placehold:string;
     private refresh:Function;
+    private accept:string;
+
+    constructor(){
+        this.accept= "";
+    }
     private getFile(pevt:{target:{files:Blob[]}}){
         this.onSelect.emit(pevt.target.files[0]);
         this.placehold = (<any>pevt.target.files[0]).name;        
