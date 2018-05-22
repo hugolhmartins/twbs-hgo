398a2ef58a4c00e6d51817cbc63bc7bbb4c6c584
Hugo Leonardo de Holanda Martins <hugo.martins@mv.com.br>
2018-05-22 15:31:41 -0300

    Add background in loading-bar. Version 0.11.4

--- src/ui/loading-bar/loading-bar.ts
+++ src/ui/loading-bar/loading-bar.ts
@@ -2,8 +2,10 @@ export class LoadingBar{
 	private isLoading:boolean;
 	private refresh:Function;
 	private barColor:string;
+	private background:string;
 	constructor(){
 		this.barColor = "#6ad7ef";
+		this.background = "";
 	}
 	private set show(on:boolean){
 		this.refresh({
