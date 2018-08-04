d0ac610455ed0d03696666d3094a80bdc24a4165
Hugo Martins <hugolhmartins@gmail.com>
2018-08-04 17:54:39 -0300

    New version 1.0.0

--- src/ui/confirm-dialog/confirm-dialog.ts
+++ src/ui/confirm-dialog/confirm-dialog.ts
@@ -1,11 +1,2 @@
-import { EventEmitter, IEventSubscribe } from "event-emitter-lite";
-
 export class ConfirmDialog{
-	public onConfirm = new EventEmitter<boolean>();
-	private confirm(){
-		this.onConfirm.emit(true);
-	}
-	private detached(){
-		this.onConfirm.unsubscribeAll();
-	}
 }
